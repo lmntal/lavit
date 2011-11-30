da7dbbdf5ae9941351a08c2dd757491c8e3a726d
Yuuki Shinobu <shinobu@ueda.info.waseda.ac.jp>
2011-11-30 19:08:09 +0000

    ウィンドウ位置情報の保存に関する修正（shinobu）

--- src/lavit/frame/MainFrame.java
+++ src/lavit/frame/MainFrame.java
@@ -35,9 +35,11 @@
 
 package lavit.frame;
 
-import java.awt.Frame;
+import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Window;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.HashSet;
 
 import javax.swing.JFrame;
@@ -45,25 +47,59 @@ import javax.swing.JSplitPane;
 import javax.swing.WindowConstants;
 
 import lavit.Env;
-import lavit.FrontEnd;
 import lavit.editor.EditorPanel;
-import lavit.util.CommonFontUser;
 
+@SuppressWarnings("serial")
 public class MainFrame extends JFrame{
-
+	
 	public HashSet<Window> childFrames;
 
 	public MainMenuBar mainMenuBar;
 	public EditorPanel editorPanel;
 	public JSplitPane jsp;
 	public ToolTab toolTab;
+	
+	private Dimension sizeSave;
+	private Point locationSave;
 
-	public MainFrame(){
-
+	public MainFrame()
+	{
 		childFrames = new HashSet<Window>();
-
-		setSize(Env.getInt("WINDOW_WIDTH"),Env.getInt("WINDOW_HEIGHT"));
-        setLocation(new Point(Env.getInt("WINDOW_X"),Env.getInt("WINDOW_Y")));
+		
+		sizeSave = new Dimension(Env.getInt("WINDOW_WIDTH"), Env.getInt("WINDOW_HEIGHT"));
+		setSize(sizeSave);
+		
+		locationSave = new Point(Env.getInt("WINDOW_X"),Env.getInt("WINDOW_Y"));
+		setLocation(locationSave);
+		
+		String state = Env.get("WINDOW_STATE");
+		if (state != null && state.equalsIgnoreCase("maximized"))
+		{
+			setExtendedState(JFrame.MAXIMIZED_BOTH);
+		}
+		
+		addComponentListener(new ComponentAdapter()
+		{
+			public void componentResized(ComponentEvent e)
+			{
+				if (getExtendedState() == JFrame.NORMAL)
+				{
+					sizeSave = getSize();
+				}
+			}
+			
+			public void componentMoved(ComponentEvent e)
+			{
+				if (getExtendedState() == JFrame.NORMAL)
+				{
+					if (getX() > 0 && getY() > 0)
+					{
+						locationSave = getLocation();
+					}
+				}
+			}
+		});
+		
         setTitle(Env.APP_NAME);
         setIconImage(Env.getImageOfFile(Env.IMAGEFILE_ICON));
         setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
@@ -79,7 +115,8 @@ public class MainFrame extends JFrame{
 		jsp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, editorPanel, toolTab);
 		jsp.setOneTouchExpandable(true);
         jsp.setResizeWeight(0.5);
-        jsp.setDividerLocation((int)(Env.getInt("WINDOW_WIDTH")*editerPer));
+        //jsp.setDividerLocation((int)(Env.getInt("WINDOW_WIDTH")*editerPer));
+        jsp.setDividerLocation((int)(getWidth()*editerPer));
         setContentPane(jsp);
 
         addWindowListener(new MainWindowListener(this));
@@ -108,13 +145,14 @@ public class MainFrame extends JFrame{
 		}
 		super.dispose();
 	}
-
-	public void exit(){
-		Env.set("WINDOW_X", getLocation().x);
-		Env.set("WINDOW_Y", getLocation().y);
-		Env.set("WINDOW_WIDTH", getWidth());
-		Env.set("WINDOW_HEIGHT", getHeight());
+	
+	public void exit()
+	{
+		Env.set("WINDOW_X", locationSave.x);
+		Env.set("WINDOW_Y", locationSave.y);
+		Env.set("WINDOW_WIDTH", sizeSave.width);
+		Env.set("WINDOW_HEIGHT", sizeSave.height);
+		Env.set("WINDOW_STATE", getExtendedState() == JFrame.MAXIMIZED_BOTH ? "maximized" : "normal");
 		Env.setPercentage("WINDOW_EDITER_PERCENTAGE",(double)jsp.getDividerLocation()/(double)getWidth());
 	}
-
 }

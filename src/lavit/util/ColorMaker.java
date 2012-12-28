9dd4854c6ee09d155c7c99983a6b81a5941e5d8f
Yuuki Shinobu <shinobu@ueda.info.waseda.ac.jp>
2012-12-28 09:54:10 +0000

    Refactored lavit.util.ColorMaker

--- src/lavit/util/ColorMaker.java
+++ src/lavit/util/ColorMaker.java
@@ -37,50 +37,64 @@ package lavit.util;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.List;
 
-public class ColorMaker {
-	static ArrayList<Color> colors = null;
+public final class ColorMaker
+{
+	private static List<Color> colors;
 
-	public static void colorPreparation(){
-		if(colors==null){
-			colors = new ArrayList<Color>();
-			for(int r : new int[]{0,128,255}){
-				for(int g : new int[]{0,128,255}){
-					for(int b : new int[]{0,128,255}){
-						colors.add(new Color(r,g,b));
-					}
+	private ColorMaker() { }
+
+	public static Color getColor(String str)
+	{
+		int s = 1;
+		for (int i = 0; i < str.length(); i++)
+		{
+			char c = str.charAt(i);
+			if (isNumber(c))
+			{
+				if (i >= 1 && isNumber(str.charAt(i - 1))) continue;
+				s += 1;
+			}
+			else if (c == '-')
+			{
+				s += 0;
+			}
+			else
+			{
+				s += c;
+			}
+		}
+		return get(s);
+	}
+
+	private static Color get(int i)
+	{
+		if (colors == null)
+		{
+			prepare();
+		}
+		return colors.get(i % colors.size());
+	}
+
+	private static void prepare()
+	{
+		colors = new ArrayList<Color>();
+		int[] a = { 0, 128, 255 };
+		for (int r : a)
+		{
+			for (int g : a)
+			{
+				for (int b : a)
+				{
+					colors.add(new Color(r, g, b));
 				}
 			}
 		}
 	}
 
-	public static Color getColor(int i){
-		colorPreparation();
-		return colors.get(i%colors.size());
+	private static boolean isNumber(char c)
+	{
+		return '0' <= c && c <= '9';
 	}
-
-	public static Color getColor(String str){
-		int s=1;
-		for(int i=0;i<str.length();++i){
-			char c = str.charAt(i);
-			if(isNumber(c)){
-				if(i>=1&&isNumber(str.charAt(i-1))){ continue; }
-				s+=1;
-			}else if(c=='-'){
-				s+=0;
-			}else{
-				s+=c;
-			}
-		}
-		return getColor(s);
-	}
-
-	public static boolean isNumber(char c){
-		if('0'<=c&&c<='9'){
-			return true;
-		}else{
-			return false;
-		}
-	}
-
 }

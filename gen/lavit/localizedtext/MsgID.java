91a354948b0f2c6c241cd0969599da30800a81b8
Yuuki Shinobu <shinobu@ueda.info.waseda.ac.jp>
2014-03-28 20:34:42 +0900

    Add generated files

---
+++ gen/lavit/localizedtext/MsgID.java
@@ -0,0 +1,144 @@
+/* This file is auto-generated. */
+package lavit.localizedtext;
+
+public final class MsgID
+{
+	/** Reboot */
+	public static final int menu_reboot = 0;
+	/** Changed */
+	public static final int text_changed = 1;
+	/** Save As... */
+	public static final int menu_saveas = 2;
+	/** LMNtal Intermediate Code */
+	public static final int text_lmntal_il = 3;
+	/** Please restart LaViT for the c... */
+	public static final int text_please_reboot = 4;
+	/** Install SLIM. */
+	public static final int text_install_slim = 5;
+	/** Yes */
+	public static final int text_yes = 6;
+	/** Use SLIM included in LaViT. */
+	public static final int text_included_slim = 7;
+	/** Installation failed. */
+	public static final int text_install_failed = 8;
+	/** Close */
+	public static final int menu_close = 9;
+	/** Language */
+	public static final int menu_lang = 10;
+	/** General Setting */
+	public static final int menu_general_setting = 11;
+	/** Version */
+	public static final int menu_version = 13;
+	/** Cut */
+	public static final int menu_cut = 12;
+	/** File System */
+	public static final int menu_item_filesystem = 14;
+	/** END */
+	public static final int text_end = 15;
+	/** Edit */
+	public static final int menu_edit = 16;
+	/** Clear */
+	public static final int button_clear = 17;
+	/** file */
+	public static final int text_file = 19;
+	/** Redo */
+	public static final int menu_redo = 18;
+	/** Reverse */
+	public static final int text_reverse = 20;
+	/** Undo */
+	public static final int menu_undo = 21;
+	/** Cygwin PATH */
+	public static final int menu_cygwin_path = 22;
+	/** File */
+	public static final int menu_file = 23;
+	/** Help */
+	public static final int menu_help = 24;
+	/** StateViewer */
+	public static final int button_stateviewer = 25;
+	/** Kill */
+	public static final int button_kill = 26;
+	/** Use already installed SLIM. */
+	public static final int text_use_already_installed = 27;
+	/** View */
+	public static final int menu_group_view = 28;
+	/** Exit */
+	public static final int menu_exit = 29;
+	/** Open... */
+	public static final int menu_open = 30;
+	/** Setting */
+	public static final int menu_setting = 31;
+	/** Run Intermediate Code (SLIM) */
+	public static final int menu_item_run_slim = 32;
+	/** Change */
+	public static final int text_change = 33;
+	/** Run */
+	public static final int menu_run = 34;
+	/** UNYO(2G) */
+	public static final int button_unyo_2g = 35;
+	/** no file */
+	public static final int text_no_file = 36;
+	/** Paste */
+	public static final int menu_paste = 37;
+	/** Java Runtime Info */
+	public static final int menu_jre_info = 38;
+	/** In Windows, Cygwin path is req... */
+	public static final int text_cygwin_required = 39;
+	/** Editing... */
+	public static final int text_editing = 40;
+	/** StateProfiler */
+	public static final int button_stateprofiler = 41;
+	/** LTL2BA is not found. */
+	public static final int text_ltl2ba_not_found = 42;
+	/** Recent Files */
+	public static final int menu_recent = 43;
+	/** $1 already exists. Do you want... */
+	public static final int text_already_exists = 44;
+	/** Run Intermediate Code (LMNtal ... */
+	public static final int menu_run_il_java = 45;
+	/** LMNtal(Java) */
+	public static final int button_lmntal_java = 49;
+	/** Close All */
+	public static final int menu_closeall = 48;
+	/** $1 has been modified. Save the... */
+	public static final int text_modified = 46;
+	/** OK */
+	public static final int text_ok = 47;
+	/** LaViT Web Page */
+	public static final int menu_webpage = 50;
+	/** Input SLIM path. */
+	public static final int text_input_slim_path = 51;
+	/** Installation completed. */
+	public static final int text_install_completed = 52;
+	/** UNYO(3G) */
+	public static final int button_unyo_3g = 53;
+	/** New UNYO UNYO is unavailable n... */
+	public static final int text_unyo_unavailable = 54;
+	/** Browse... */
+	public static final int button_browse = 55;
+	/** Cancel */
+	public static final int text_cancel = 58;
+	/** Open StateViewer File */
+	public static final int menu_open_sv_file = 57;
+	/** LaViT need SLIM path. */
+	public static final int text_need_slim_path = 56;
+	/** New */
+	public static final int menu_new = 59;
+	/** SLIM PATH */
+	public static final int menu_slim_path = 61;
+	/** (Empty) */
+	public static final int menu_recent_empty = 60;
+	/** Clear */
+	public static final int menu_clear_recent = 62;
+	/** No */
+	public static final int text_no = 63;
+	/** Copy */
+	public static final int menu_copy = 64;
+	/** Save */
+	public static final int menu_save = 66;
+	/** (unavailable) */
+	public static final int text_unavailable = 65;
+	/** overwrite */
+	public static final int text_overwrite = 67;
+	/** SLIM */
+	public static final int button_slim = 68;
+}

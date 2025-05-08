e281818e9bec0309d9cbb944c7788b5a54ca492b
yuta <seelx3@gmail.com>
2025-05-08 20:59:51 +0900

    update version info

--- src/lavit/Env.java
+++ src/lavit/Env.java
@@ -74,7 +74,7 @@ import lavit.util.StringUtils;
 
 public final class Env {
 	public static final String APP_NAME = "LaViT";
-	public static final String APP_VERSION = "2.11.1";
+	public static final String APP_VERSION = "2.12.0";
 	public static final String APP_DATE = "2024/06/17";
 	public static final String APP_HREF = "http://www.ueda.info.waseda.ac.jp/lmntal/lavit/";
 

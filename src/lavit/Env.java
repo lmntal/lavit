fca4c2893db09af0918c47fb31b8df5091136777
seelx3 <seelx3@gmail.com>
2025-05-15 23:55:03 +0900

    chore: update jar & version info

--- src/lavit/Env.java
+++ src/lavit/Env.java
@@ -75,7 +75,7 @@ import lavit.util.StringUtils;
 public final class Env {
 	public static final String APP_NAME = "LaViT";
 	public static final String APP_VERSION = "2.12.0";
-	public static final String APP_DATE = "2024/06/17";
+	public static final String APP_DATE = "2025/05/15";
 	public static final String APP_HREF = "http://www.ueda.info.waseda.ac.jp/lmntal/lavit/";
 
 	public static final String LMNTAL_VERSION = "LMNtal : 1.60 (2023/04/03)";
